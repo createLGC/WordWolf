1ed07e28a1629e9750f27c6855a9a5b6e9aee4d8
6C106 <6C106@DESKTOP-7SB510O>
2021-07-07 16:46:35 +0900

    commit

---
+++ WordWolf/src/main/java/model/Themes.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Themes {
+	private String personTheme;
+	private String wolfTheme;
+	
+	public Themes(String personTheme, String wolfTheme) {
+		this.personTheme = personTheme;
+		this.wolfTheme = wolfTheme;;
+	}
+	public String getTheme(String person, String wolf) { return personTheme; }
+
+}
