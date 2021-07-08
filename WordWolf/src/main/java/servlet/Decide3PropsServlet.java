ea33b8b3c49caed5c478942bc2f8b944749f97c6
6C113 <6C113@DESKTOP-QMNRDJ3>
2021-07-08 16:30:33 +0900

    コミット

--- WordWolf/src/main/java/servlet/Decide3PropsServlet.java
+++ WordWolf/src/main/java/servlet/Decide3PropsServlet.java
@@ -1,11 +1,15 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.Game;
 
 /**
  * Servlet implementation class Decide3PropsServlet
@@ -19,6 +23,26 @@ public class Decide3PropsServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		//numOfWolves, talkTime, themeTypeをgetParameterで取得。
+		
+		String numOfWolves = request.getParameter("numOfWolves");
+		String talkTime = request.getParameter("talkTime");
+		String themeType = request.getParameter("themeType");
+		
+		int strInt = Integer.parseInt(numOfWolves);
+		
+		//セッションスコープからGameインスタンスを取得。
+		Game g = (Game)request.getSession().getAttribute("game");
+	
+		g.setWolves(strInt);
+		g.setTalkTime(talkTime);
+		g.setThemes(themeType);
+		
+		//フォワード
+		RequestDispatcher dispatcher = 
+				request.getRequestDispatcher
+				("/jsp/noticeThemes.jsp");
+		
+		dispatcher.forward(request, response);
 	}
-
 }
