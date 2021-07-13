002cee204d7a3c660e8542aefbe257568fe5f0de
6C107 <6C107@DESKTOP-LCMVACG>
2021-07-13 10:50:09 +0900

    test.java

--- WordWolf/src/main/java/dao/ThemeDAOTest.java
+++ WordWolf/src/main/java/dao/ThemeDAOTest.java
@@ -18,10 +18,8 @@ class ThemeDAOTest {
 		MatcherAssert.assertThat(this.findAllFromCSV(), hasItems(ThemeDAO.find("ブランド").toArray(new String[0])));
 	}
 	
-	
-	
 	private List<String> findAllFromCSV() {
-		List<String> themeTypeList = new ArrayList<>();
+		List<String> themeList = new ArrayList<>();
 		
 		try (
 			FileInputStream fi = new FileInputStream("src/main/sql/theme.csv");
@@ -44,13 +42,13 @@ class ThemeDAOTest {
 					arr = line.split(",");
 				} else {
 					String[] data = line.split(",");
-					themeTypeList.add(data[1]);
+					themeList.add(data[2]);
 				}
 				//行数のインクリメント
 				i++;	
 			}
 			
-			return themeTypeList;
+			return themeList;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
