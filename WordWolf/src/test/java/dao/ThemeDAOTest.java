6da71e1cb0f047b4cd8e3b26d8f7cc4ee7e3b8a7
6C106 <6C106@DESKTOP-7SB510O>
2021-07-20 16:22:59 +0900

    commit

--- WordWolf/src/test/java/dao/ThemeDAOTest.java
+++ WordWolf/src/test/java/dao/ThemeDAOTest.java
@@ -3,6 +3,7 @@ package dao;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,7 +14,7 @@ import org.junit.jupiter.api.Test;
 class ThemeDAOTest {
 
 	@Test
-	void testFind() {
+	void testFind() throws SQLException {
 		MatcherAssert.assertThat(this.findAllFromCSV(), CoreMatchers.hasItems(ThemeDAO.find("動物").toArray(new String[0])));
 	}
 
